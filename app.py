5f3ec3e7009ff44400f3ffc3ef073cb6ae2cd2ea
Vedant <122482472+vedantparmar12@users.noreply.github.com>
2024-08-08 16:26:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,34 +3,27 @@ import pickle
 import string
 from nltk.corpus import stopwords
 import nltk
-import nltk
-nltk.download('punkt')  # Download the 'punkt' resource
+nltk.download('punkt')
+nltk.download('stopwords')
 from nltk.stem.porter import PorterStemmer
-
 ps = PorterStemmer()
 
 def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
-
     y = []
     for i in text:
         if i.isalnum():
             y.append(i)
-
     text = y[:]
     y.clear()
-
     for i in text:
         if i not in stopwords.words('english') and i not in string.punctuation:
             y.append(i)
-
     text = y[:]
     y.clear()
-
     for i in text:
         y.append(ps.stem(i))
-
     return " ".join(y)
 
 # Load the vectorizer and the trained model
@@ -38,7 +31,6 @@ tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 
 st.title("Email/SMS Spam Classifier")
-
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
